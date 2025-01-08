0cef4ea0004a4be6ad4081670a310f5b69017126
Feyorra <hi@feyorra.com>
2025-01-08 12:33:56 +0200

    update constructor parameters and change burn and mint methods visibility

--- contracts/FEYToken.sol
+++ contracts/FEYToken.sol
@@ -4,20 +4,18 @@ pragma solidity ^0.8.24;
 
 import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {Ownable} from "./Ownable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract FEYToken is ERC20, ERC20Permit, Ownable {
     constructor(
-        uint256 initialSupply
-    ) public ERC20("Feyorra", "FEY") ERC20Permit("Feyorra") {
-        _mint(_msgSender(), initialSupply);
+        address _bridge
+    ) ERC20("Feyorra", "FEY") ERC20Permit("Feyorra") Ownable(_bridge) {}
+
+    function burn(uint256 _amount) external onlyOwner {
+        _burn(_msgSender(), _amount);
     }
 
-    function burn(uint256 value) public onlyOwner {
-        _burn(_msgSender(), value);
-    }
-
-    function mint(uint256 value) public onlyOwner {
-        _mint(_msgSender(), value);
+    function mint(uint256 _amount) external onlyOwner {
+        _mint(_msgSender(), _amount);
     }
 }
